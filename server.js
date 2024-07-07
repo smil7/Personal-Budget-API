af6c43385594986bbbaef69e300fab4928ad0751
Smily Os <osama2231@hotmail.com>
2024-07-07 16:38:49 +0400

    Added morgan library to server and gitignore file

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
   
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(morgan('dev'));
 app.use(bodyParser.json());
+const apiRouter = require('./back-end/api');
+app.use('/api', apiRouter);
 
 app.get('/', (req, res) => {
   res.send('<h1>Welcome to the Personal Budget app!</h1>');
 });
-
-const apiRouter = require('./back-end/api');
-app.use('/api', apiRouter);
   
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
