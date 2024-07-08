c913891990c0699f93889c9aac2f755df65c1c32
Smily Os <osama2231@hotmail.com>
2024-07-08 22:13:52 +0400

    Added a PUT and POST requests that allow you to update a single category details.

--- back-end/routes/envelopeRouter.js
+++ back-end/routes/envelopeRouter.js
@@ -1,23 +1,48 @@
 const express = require('express');
 const envelopeRouter = express.Router();
-const {addToDatabase, getAllData, getCategoryByName} = require('../utils/db-functions');
+const {addToDatabase, getAllData, getCategoryById, updateCategoryDetails, subtractBudgetValue} = require('../utils/db-functions');
+
+envelopeRouter.param('categoryId', (req, res, next, id) => {
+    const categoryId = Number(id);
+    const category = getCategoryById(categoryId);
+    if(category === undefined){
+        res.status(404).send('Category not found!');
+    }
+    else{
+        req.category = category;
+        next();
+    }
+})
 
 envelopeRouter.post('/', (req, res, next) => {
     const newEnvelope = addToDatabase(req.body);
     res.status(201).send(newEnvelope);
 });
 
+envelopeRouter.post('/:categoryId', (req, res, next) => {
+    let budget = subtractBudgetValue(req.body);
+    if(budget === null){
+        return res.status(400).send(budget);
+    }
+    return res.status(200).send(budget);
+})
+
 envelopeRouter.get('/', (req, res, next) => {
     const envelopesCategories = getAllData();
     res.status(200).send(envelopesCategories);
 });
 
-envelopeRouter.get('/:categoryName', (req, res, next) => {
-    const category = getCategoryByName(req.params.categoryName);
-    if(category === null){
-        return res.status(404).send(`${req.params.categoryName} not in the database.`)
-    }
+envelopeRouter.get('/:categoryId', (req, res, next) => {
+    const category = req.category;
     res.status(200).send(category);
-})
+});
+
+envelopeRouter.put('/:categoryId', (req, res, next) => {
+    let updatedCategory = updateCategoryDetails(req.body);
+    if(updatedCategory === null){
+        return res.status(400).send('Nothing updated, because same details!');
+    }
+    return res.status(200).send(updatedCategory);
+});
 
 module.exports = envelopeRouter;
--- back-end/utils/db-functions.js
+++ back-end/utils/db-functions.js
@@ -5,24 +5,19 @@ let categoryIdCounter = budgetCategories.length+1;
 const contains = (newCategory) => {
     for (const [key, value] of Object.entries(budgetCategories)){
         if(value.category === newCategory){
-            return false;
+            return true;
         }
     }
-    return true;
+    return false;
 }
 
-const getCategory = (categoryName) => {
-    for (const [key, value] of Object.entries(budgetCategories)){
-        if(value.category === categoryName){
-            return value;
-        }
-    }
-    return null;
+const getCategory = (categoryId) => {
+    return budgetCategories.findIndex(category => category.id === categoryId);
 }
 
 const addToDatabase = (instance) => {
     const found = contains(instance.category);
-    if(!found){
+    if(found){
         return null;
     }
     else{
@@ -36,9 +31,37 @@ const getAllData = () => {
     return budgetCategories;
 }
 
-const getCategoryByName = (instance) => {
-    const category = getCategory(instance);
-    return category;
+const getCategoryById = (categoryId) => {
+    const categoryIndex = getCategory(categoryId);
+    return budgetCategories[categoryIndex];
 }
 
-module.exports = {addToDatabase, getAllData, getCategoryByName};
+// Updates the category's details by the PUT request
+const updateCategoryDetails = (instance) => {
+    const categoryIndex = getCategory(instance.id);
+
+    if(instance.category !== budgetCategories[categoryIndex].category){
+        budgetCategories[categoryIndex].category = instance.category;
+    }
+    if(instance.budget !== budgetCategories[categoryIndex].budget){
+        budgetCategories[categoryIndex].budget = instance.budget;
+    }
+
+    return budgetCategories[categoryIndex];
+}
+
+// Updates the value of the budget by the POST request
+const subtractBudgetValue = (instance) => {
+    const categoryIndex = getCategory(instance.id);
+
+    if(instance.category === budgetCategories[categoryIndex].category){
+        if(!(instance.budget > budgetCategories[categoryIndex].budget)){
+            budgetCategories[categoryIndex].budget = Math.abs(instance.budget - budgetCategories[categoryIndex].budget);
+            return budgetCategories[categoryIndex];
+        }
+        return 'The amount sent exceeds the envelope\'s budget!';
+    }
+    return 'The envelope\'s category name is not the same!';
+}
+
+module.exports = {addToDatabase, getAllData, getCategoryById, updateCategoryDetails, subtractBudgetValue};
